34b2e498852e2e4a4d54916e8e6ebea05924360d
Evgeiy <Shmulev06@gmail.com>
2022-12-14 21:53:29 +0200

    button hidden

--- src/style.js
+++ src/style.js
@@ -1,10 +1,20 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor")
+const mainElement = document.querySelector(".main-class");
+const HIDDEN= "hidden";
+function showDetails(){
+mainElement.classList.remove("hidden");
+}
+function hideDetails(){
+mainElement.classList.add("hidden");
+}
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
-    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    showDetails();
+    const title = anchor.getAttribute("data-details-title");
+    detailsTitle.innerHTML = title;
 
 }
 for (let i = 0; i < anchorElements.length; i++) {
