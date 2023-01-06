dfc06f76590cb873d0c18b35c97061472f4de226
Evgeiy <Shmulev06@gmail.com>
2023-01-06 09:06:43 +0200

    hw#14

--- app.js
+++ app.js
@@ -1,78 +1,49 @@
-// const ar = [] ;
-// const ar1 = [1,2,3,4,]
-// ar.push(...ar1);
-// ar.push('abc');
-function getRandomNumber(min, max) {
-   
-    return min + Math.trunc(Math.random() * (max - min + 1));
-}
-function getRandomMatrix(rows, columns, min, max){
-    const matrix = [];
-    for(let i = 0; i < rows; i++) {
-        matrix.push([]);
-        for (let j = 0; j < columns; j++) {
-            matrix[i].push(getRandomNumber(min, max))
-        }
-    }
-    return matrix;
-}
-//const matrix = getRandomMatrix(3, 4, 0, 1);
-const ar10 = [1,2,3,4,5];
-const str =ar10.join('_');
+const array=[10,20,30,-10,11,100];
+//array.splice(2,2);
+const index = array.indexOf(30);
+//remove all numbers from 30
+//const res = array.splice(index,10);
 
-//HTML
-//<ul class="list_class">
-//  <li class="item_class">
-//     <div class="white"> </div>
-//  </li>
-// .............
-//<li class="item_class">
-//     <div class="black"> </div>
-//  </li>
-//</ul>
-function getHtmlUl(array) {
-   
-    return `<ul class="list_class">
-      ${getListItems(array)}
-      </ul>`;
-}
-function getListItems(array) {
-    const res = [];
-    for(let i = 0; i < array.length; i++) {
-        res[i] = `
-                <li class="item_class">
-                    <div class="${array[i] === 0 ? "white" : "black"}">
-                     </div>
-                 </li>`
-    }
-    return res.join('');
-}
-const strClass = getRandomNumber(0, 1) === 0 ? 'white' : 'black';
-const str1 = "hello world";
-const str2 = 'hello ' + '"world"';
-const str3 = `class="${strClass}"`;
-function matrixTransp(matrix) {
-    //TODO
-    //returns transp matrix
-    // matrix = [[1, 2], //input
-    //           [3, 4], 
-    //           [4, 5]        
-    //            ]
-    //  output   [1, 3, 4]
-    //           [2, 4, 5] 
-    const res =[];
-    for(let i = 0; i < matrix[0].length; i++){
-        res[i] = [];
-        for(let j = 0; j < matrix.length; j++) {
-            res[i][j] = matrix[j][i];
-        }
-    }
-    return res;
-}
-const matrix = [[1, 2], 
-              [3, 4], 
-              [4, 5]        
-               ];
-//console.log(matrixTransp(matrix));
-console.log(getHtmlUl([0,1,1,0,0]))
 
+//insert
+//array.splice(index,0,-10,-20)
+
+//replace
+//array.splice(index,2,20);
+// const ar1 = [40,200];
+// array.splice(index,1,...ar1);
+// const numbersPerPage=2;
+// const page=2;
+// const firsiIndex =(page-1)*numbersPerPage;
+// const pageNumbers = array.slice(firsiIndex,firsiIndex+numbersPerPage); 
+
+
+
+// const ar1 = array.map(multiply2);
+// function multiply2(number){
+//     return number*2;
+// }
+// const ar2 = array.map(function(number,index){
+//     return index+1+'. ' + number;
+
+// })
+// const ar3 = array.map(function(number){
+// return `<li>${number}</li>`;
+// })
+// const ulElement = `<ul>${ar3.join('')} </ul>`
+
+
+
+//filter function (getting array of numbers in a given rage)
+
+// const min=5;
+// const max=15;
+// const ar4 = array.filter(function(number){
+//     return number>=min && number <=max;
+// })
+
+//getting array of the elements of the sourse array with even
+
+const ar5 = array.filter(function(__,index){
+    return index%2===0;
+})
